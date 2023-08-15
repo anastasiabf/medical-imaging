a95fc9a0ca3486f431bdd727e94ce4f5229c44cc
Anastasia Berlianna <99186138+anastasiabf@users.noreply.github.com>
2023-08-15 17:07:50 +0700

    Add files via upload

---
+++ Multimodal Biomedical Imaging Final Project/07311940000004_app_cnn.py
@@ -0,0 +1,250 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F          
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader  
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (10, 4)
+
+def show_mnist_images(images, labels):
+    # Menampilkan citra-citra
+    grid_image = make_grid(images[:12], nrow=12)
+    grid_image = grid_image.permute(1, 2, 0)  # Ubah urutan dimensi menjadi (Width, Height, Color)
+
+    # Menampilkan label
+    st.write("Labels: ", labels[:12])
+
+    # Menampilkan gambar menggunakan Streamlit
+    st.image(np.array(grid_image))
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, 1) #1color channel, 16 filters (output channel from layer1), 3x3 kernel, stride=1
+        self.conv2 = nn.Conv2d(16, 32, 3, 1) #16 in channels, 32 out channels
+        self.conv3 = nn.Conv2d(32, 64, 3, 1) #32 in channels, 64 out channels
+        self.fc1 = nn.Linear(1*1*64, 120) #for fully connected layer, dgn 120 neurons
+        self.fc2 = nn.Linear(120, 84) #120 input dari node fc1, jumlah neuron berikutnya dikurangi
+        self.fc3 = nn.Linear(84,10) #output layer dari fully connected
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2) #input=X, kernel=2x2, stride=2
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv3(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 1*1*64) #-1 agar kita bisa memvariasikan batch size
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1) #fungsi aktivasi bagian akhir, dimension=1 karena sudah 1 array flatten
+
+def preprocess_image(image):
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((28, 28)),
+        transforms.ToTensor()
+    ])
+    preprocessed_image = transform(image)
+    preprocessed_image = preprocessed_image.unsqueeze(0)  # Menambahkan dimensi batch
+    return preprocessed_image
+
+def main():
+    # logo
+    logo = Image.open("D:\KULIAH\TA\GUI\logo.png")
+    st.image(logo, width = 100)
+    st.title("Classification MNIST Dataset Using Convolutional Neural Network")
+
+    menu = ["Home", "Train and Test", "Predict"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    transform = transforms.ToTensor()
+    cwd = os.getcwd()
+    data_path = os.path.join(cwd, 'MNIST')
+
+    train_data = datasets.MNIST(root=data_path, train=True, download=True, transform=transform)
+    test_data = datasets.MNIST(root=data_path, train=False, download=True, transform=transform)
+
+    model = ConvolutionalNetwork()
+
+    if choice == "Home":
+        st.header('Tugas Multimodal Biomedical Image Analysis')
+        st.subheader('Anastasia Berlianna Febiola - 07311940000004')
+
+    if choice == "Train and Test":
+         st.header("Train and Validation on Dataset")
+         bs_tr = st.sidebar.slider("Batch Size Train", 50, 1000, step=50)
+         bs_ts = st.sidebar.slider("Batch Size Test", 50, 1000, step=50)
+         ep = st.sidebar.number_input("Epoch", min_value=5, max_value=100)
+         LR = st.sidebar.selectbox("Learning Rate", [0.00001, 0.0001, 0.001, 0.01])
+
+         st.write("Dataset Type : {}".format(type(train_data[0])))
+         st.subheader("Dataset")
+         train_loader = DataLoader(train_data, batch_size=bs_tr, shuffle=True)
+         for images, labels in train_loader:
+             show_mnist_images(images, labels)
+             break
+         
+         st.info("Selected Hyperparameters:\n"
+                "- Batch Size Train: {}\n"
+                "- Batch Size Test: {}\n"
+                "- Epoch: {}\n"
+                "- Learning Rate: {}".format(bs_tr, bs_ts, ep, LR))
+
+         if st.sidebar.button("Start Train", use_container_width=True):
+             torch.manual_seed(101)
+             train_loader = DataLoader(train_data, batch_size=bs_tr, shuffle=True)
+             test_loader = DataLoader(test_data, batch_size=bs_ts, shuffle=False)
+             criterion = nn.CrossEntropyLoss()
+             optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+             start_time = time.time()
+             epochs = ep
+
+             train_losses = []
+             test_losses = []
+             train_correct = []
+             test_correct = []
+
+             for i in range(epochs):
+                trn_corr = 0 #train correct currently
+                tst_corr = 0 #test correst
+    
+                # Run the training batches
+                # with enumerate, we're actually going to keep track of what batch number we're on with B.
+                for b, (X_train, y_train) in enumerate(train_loader): #y_train=output = label, b = batches, train_loader = return back the image and its label
+                    b+=1
+        
+                    # Apply the model
+                    y_pred = model(X_train)  # Here we flatten X_train
+                    loss = criterion(y_pred, y_train) #calculating error difference
+ 
+                    # calculate the number of correct predictions
+                    predicted = torch.max(y_pred.data, 1)[1] #check print(y_pred.data) to know data of one epoch, 1 = actual predicted value
+                    batch_corr = (predicted == y_train).sum()
+                    trn_corr += batch_corr
+        
+                    # Update parameters
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                    # Print results per epoch
+                    if b == len(train_loader) - 1:
+                        st.write(f"Epoch: {i+1}/{epochs} | Batch: {b+1}/{len(train_loader)} | Loss: {loss.item():.4f} | Accuracy: {trn_corr.item()}/{len(train_data)} = {trn_corr.item()*100/len(train_data):.2f}%")
+                
+                # Update train loss & accuracy for the epoch
+                train_losses.append(loss.item())
+                train_correct.append(trn_corr.item())
+
+                # Run the testing batches
+                with torch.no_grad(): #don't update weight and bias in test data
+                    for b, (X_test, y_test) in enumerate(test_loader):
+
+                    # Apply the model
+                     y_val = model(X_test)  
+
+                     # Calculating the number of correct predictions
+                     predicted = torch.max(y_val.data, 1)[1] 
+                     tst_corr += (predicted == y_test).sum()
+
+                # Update test loss & accuracy for the epoch
+                loss = criterion(y_val, y_test)
+                test_losses.append(loss)
+                test_correct.append(tst_corr)
+
+             # Calculate training time
+             end_time = time.time()
+             training_time = end_time - start_time
+             st.text(f"Training time: {training_time:.2f} seconds")
+             # Save the trained model
+             torch.save(model.state_dict(), 'modelcnn.pth')
+
+             # Plot train and test loss
+
+             plt.plot(train_losses, label='Train Loss')
+             plt.plot(test_losses, label='Test Loss')
+             plt.xlabel('Epoch')
+             plt.ylabel('Loss')
+             plt.title('Train and Test Loss')
+             plt.legend()
+             plt.savefig('loss_plot.png')  # Save the plot as an image
+
+             # Plot train and test accuracy
+             train_accuracy = [100 * correct / len(train_data) for correct in train_correct]
+             test_accuracy = [100 * correct / len(test_data) for correct in test_correct]
+
+             plt.figure()
+             plt.plot(train_accuracy, label='Train Accuracy')
+             plt.plot(test_accuracy, label='Test Accuracy')
+             plt.xlabel('Epoch')
+             plt.ylabel('Accuracy (%)')
+             plt.title('Train and Test Accuracy')
+             plt.legend()
+             plt.savefig('accuracy_plot.png')  # Save the plot as an image
+
+             # Show the saved plots using Streamlit
+             st.image('loss_plot.png', caption='Train and Test Loss')
+             st.image('accuracy_plot.png', caption='Train and Test Accuracy')
+
+
+         if st.sidebar.button("Start Test", use_container_width=True):
+            # Load the saved model
+            model.load_state_dict(torch.load('modelcnn.pth'))
+
+            # Extract the data all at once, not in batches
+            test_load_all = DataLoader(test_data, batch_size=10000, shuffle=False)
+
+            with torch.no_grad():
+                correct = 0
+                for X_test, y_test in test_load_all:
+                    y_val = model(X_test)  # pass in a flattened view of X_test
+                    predicted = torch.max(y_val,1)[1]
+                    correct += (predicted == y_test).sum()
+                st.header('Test on Dataset Result')
+                st.markdown(f'**Test accuracy: {correct.item()}/{len(test_data)} = :red[{correct.item()*100/(len(test_data)):7.3f}%]**')
+
+    elif choice == "Predict":
+        st.header('Prediction Result on Image')
+        st.write(type(test_data))
+        index = st.sidebar.slider("Select Image Index", 0, len(test_data) - 1)
+        image, label = test_data[index]
+        image_array = torch.transpose(image, 0, 2).numpy()
+        st.image(image_array, width=150)
+
+        # Function to perform the prediction
+        def predict_class(image):
+            # Load the trained model
+            model = ConvolutionalNetwork()  
+            model.load_state_dict(torch.load('modelcnn.pth'))
+            model.eval()  # Set the model to evaluation mode
+
+            # Perform the prediction
+            with torch.no_grad():
+                output = model(image)
+
+            predicted_class = torch.argmax(output, dim=1).item()
+            return predicted_class
+
+        # Perform the prediction when the "Predict Class" button is pressed
+        if st.button("Predict Class"):
+            preprocessed_image = preprocess_image(image)
+            predicted_class = predict_class(preprocessed_image)
+            #predicted_class = predicted_class.item()  # Convert tensor to Python scalar
+            st.write(f"Predicted Class: {predicted_class}")
+
+if __name__ == '__main__':
+    main()
